d17ade55db3fdae01d8080bd21399de8b1a2a261
Hugo Silva <hugomsilva@ua.pt>
2020-08-25 14:14:32 +0100

    feat(store): create vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,47 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    labels: ['hugo'], // list of strings (avoid duplicated strings)
+    notes: []
   },
   mutations: {
+    addLabel: (state, payload) => {
+      console.log('mutation add label with payload', payload)
+      if (!state.labels || !state.labels.length) {
+        state.labels = []
+      }
+      state.labels.unshift(payload)
+    }
   },
   actions: {
+    addLabel: ({ state, commit }, payload) => {
+      console.log('action add label with payload', payload)
+
+      return new Promise((resolve, reject) => {
+        // check if note label is new or not
+        if (state.labels.includes(payload)) {
+          reject('Duplicated note label!')
+        } else {
+          commit('addLabel', payload)
+          resolve()
+        }
+      })
+    }
   },
   modules: {
+  },
+  getters: {
+    listNotes: (state) => {
+      return state.notes.filter(note => (!note.reminderDate && !note.archived && !note.deleted))
+    },
+    listReminders: (state) => {
+      return state.notes.filter(note => (note.reminderDate && !note.archived && !note.deleted))
+    },
+    listArchived: (state) => {
+      return state.notes.filter(note => (note.archived && !note.deleted))
+    },
+    listDeleted: (state) => {
+      return state.notes.filter(note => note.deleted)
+    }
   }
 })
